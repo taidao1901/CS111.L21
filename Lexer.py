059496fc2480b038e05781c164002d330b04bf3d
Dao Van Tai <19522148@gm.uit.edu.vn>
2021-06-25 10:44:16 +0700

    add comment

--- Lexer.py
+++ Lexer.py
@@ -2,19 +2,20 @@ import ply.lex as lex
 import ply.yacc as yacc
 
 
-keywords = ('else','in','for','while','elif','if','range','print','input')
+keywords = ('else','in','for','while','elif','if','range','print','input') 
 operators=('EQUALS','PLUS','MINUS','TIMES','DIVIDE','GREATER','LESS','EQUALTO','NOTEQUALTO')
 delimiters=('LPAREN','RPAREN','COLON','COMMA')
 literal=('INTEGER','FLOAT','STRING')
-tokens =keywords+operators+delimiters+literal+('ID','INDENT','DEDENT','SPACE','NEWLINE')
+# Danh sách các token cần định nghĩa
+tokens =keywords+operators+delimiters+literal+('ID','INDENT','DEDENT','SPACE','NEWLINE','COMMENT') 
 
-
+# Định nghĩa biến (Indentify)
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z0-9_]*'
     if t.value in keywords:
         t.type=t.value
     return t
-
+# Định nghĩa các operator
 t_EQUALS = r'='
 t_PLUS = r'\+'
 t_MINUS = r'-'
@@ -24,59 +25,66 @@ t_GREATER = r'>'
 t_LESS = r'<'
 t_EQUALTO = r'=='
 t_NOTEQUALTO = r'!='
-
+# Định nghĩa các delimiter
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_COLON=r'\:'
 t_COMMA= r'\,'
-
+#Định nghĩa các kiểu dữ liệu integer, float, string
 t_INTEGER = r'\d+'
 t_FLOAT= r'((\d*\.\d+)(E[\+-]?\d+)?|([1-9]\d*E[\+-]?\d+))'
 def t_STRING(t):
     r'\'([^\\\n]|(\\.))*?\''
     return t
- 
+# Định nghĩa comment, bỏ qua token này
 def t_COMMENT(t):
     r'\#.*'
     pass
-    
+ # Nếu kí hiệu không nằm trong danh sách token thì thông báo illegal character, và bỏ qua 
 def t_error(t):
     print ("illegal character '%s" %t.value[0])
     t.lexer.skip(1)
-
+# Định nghĩa newline, thụt lề, bỏ thụt lề
 indents = [0]
 def t_INDENT(t):
     r'\n[ ]*' 
     t.lexer.lineno += 1
-    if t.lexer.lexpos >= len(t.lexer.lexdata) or t.lexer.lexdata[t.lexer.lexpos] == "\n": # empty line
-        return None
+    # Bỏ qua dòng trống
+    if t.lexer.lexpos >= len(t.lexer.lexdata) or t.lexer.lexdata[t.lexer.lexpos] == "\n":
+        return None  
     width = len(t.value) - 1
     last_pos = t.lexer.lexpos - width
-    if width == indents[-1]:
+    # Nếu width cùng cấp  với thụt lề cuối cùng thì trả về Newline
+    if width == indents[-1]: 
         t.type= 'NEWLINE'
         t.value= '\n'
         return t
-        #return None
-    elif width > indents[-1]:
+    # Nếu width cùng cấp  với thụt lề cuối cùng hơn cấp thì trả về là Indent
+    elif width > indents[-1]: 
         t.type = 'INDENT'
         t.value = 1
         indents.append(width)
         return t
+    # Xét xóa thụt lề (Dedent)
     else:
         ded = 0
+        # Xét các thụt lề trước đó
         while len(indents) > 1:
             indents.pop()
             ded += 1
+            # Nếu width cùng cấp với thụt lề cuối cùng thì trả về Dedent
             if width == indents[-1]:
                 t.type = 'DEDENT'
                 t.value = ded
                 return t
         raise Exception("bad indent level at line %d: %s" % (t.lexer.lineno - 1,t.lexer.lines[t.lexer.lineno-1]))
-
+# Định nghĩa khoảng trắng, bỏ qua
 def t_SPACE(t):
     r'[ ]+'
     return None
+# Kích hoạt lexer
 lexer=lex.lex()
+
 """lines = []
 while True:
     line = input()
--- Parser.py
+++ Parser.py
@@ -7,6 +7,7 @@ precedence = (
     ('left','TIMES','DIVIDE'),
 )
 names= { }
+# Định nghĩa cụm câu lệnh
 def p_statements(p):
     '''statements : statement
             | statement NEWLINE statements
@@ -17,6 +18,7 @@ def p_statements(p):
         p[0]=p[1]
     else: 
         p[0]=(p[1],p[2])
+# Định nghĩa câu lệnh
 def p_statement(p):
     '''statement : assign
             | expr
@@ -26,9 +28,11 @@ def p_statement(p):
             | for_stmt
             | while_stmt'''
     p[0]=p[1]
+# Định nghĩa phép gán
 def p_assign(p):
     '''assign : ID EQUALS expr'''
     p[0]=('ASSIGN',p[1],p[3])
+# Định nghĩa các biểu thức
 def p_expr(p):
     '''expr : expr PLUS term
             | expr MINUS term
@@ -43,6 +47,7 @@ def p_expr(p):
             p[0]=p[2]
     elif (len(p)==2):
         p[0]=(p[1])
+# Định nghĩa term (bao gồm phép nhân, phép chia)
 def p_term(p):
     '''term : term TIMES factor
            | term DIVIDE factor
@@ -54,18 +59,22 @@ def p_term(p):
             p[0]=('/',p[1],p[3])
     elif (len(p)==2):
         p[0]=(p[1])
+# Định nghĩa factor ( bao gồm kiểu dữ liệu, biến)
 def p_factor(p):
     '''factor : INTEGER
               | ID
               | STRING
               | FLOAT'''
     p[0]= p[1]
+# Định nghĩa câu lệnh input: Indentify + '=' + '(' + ')'
 def p_input_stmt(p):
     '''input_stmt : ID EQUALS input LPAREN RPAREN'''
     p[0]=('input',p[1])   
+# Định nghĩa câu lệnh print: 'print' + '(' + Expression + ')'
 def p_print(p):
     '''print_stmt : print LPAREN expr RPAREN'''
     p[0]=('print',p[3])
+# Định nghĩa các phép so sánh: Expression +'>'+Expression | Expression + '<' + Expression | Expression + '==' + Expression | Expression + '!=' + Expression
 def p_comp_op(p):
     '''comp_op : expr GREATER expr
             | expr LESS expr
@@ -79,6 +88,7 @@ def p_comp_op(p):
         p[0]=('EQUALTO',p[1],p[3])
     elif p[2]=='!=':
         p[0]=('NOTEQUALTO',p[1],p[3])
+# Định nghĩa câu lệnh if: 'if' + comp_op + ':' + block | 'if' + comp_op + ':' + block + elif_stmt | 'if' + comp_op + ':' + block + else_block
 def p_if_stmt(p):
     '''if_stmt : if comp_op COLON block
             | if comp_op COLON block elif_stmt
@@ -87,6 +97,7 @@ def p_if_stmt(p):
         p[0]=('if',p[2],p[4])
     elif len(p)==6:
         p[0]=('if',p[2],p[4],p[5])
+# Định nghĩa câu lệnh elif: 'elif' + comp_op + ':' + block | 'elif' + comp_op + ':' + block + elif_stmt | 'elif' + comp_op + ':' + block + else_block
 def p_elif_stmt(p):
     '''elif_stmt : elif comp_op COLON block
             | elif comp_op COLON block elif_stmt
@@ -94,31 +105,39 @@ def p_elif_stmt(p):
     if len(p)==5:
         p[0]=('elif',p[2],p[4])
     elif len(p)==6:
-        p[0]=('elif',p[2],p[4],p[5])        
+        p[0]=('elif',p[2],p[4],p[5])
+# Định nghĩa câu lệnh else: 'else'+ ':'+ block        
 def p_else_block(p):
     '''else_block : else COLON block'''
     p[0]=('else',p[3])
+# Định nghĩa block: indent + statements (sau block không còn câu lệnh nào)| indent +statements+ dedent (sau block vẫn còn câu lệnh khác) 
 def p_block(p):
     '''block : INDENT statements
             | INDENT statements DEDENT'''
     p[0]=('block',p[2])
+# Định nghĩa câu lệnh for: 'for'+ indentify + 'in' + 'range' + target_list +  ':' + block
 def p_for_stmt(p):
-    '''for_stmt : for ID in range in_range COLON block '''
+    '''for_stmt : for ID in range target_list COLON block '''
     p[0]=('for',p[2],p[5],p[7])
-def p_in_range(p):
-    '''in_range : expr
+# Định nghĩa target_list: Expression | '('+ Expression + ',' + Expression ')'
+def p_target_list(p):
+    '''target_list : expr
             | LPAREN expr COMMA expr RPAREN'''
     if len(p)==2:
         p[0]=('in_range',p[1])
     elif len(p)==6:
         p[0]=('in_range',p[2],p[4])
+# Định nghĩa câu lệnh while: 'while' + comp_op + ':' + block
 def p_while_stmt(p):
     '''while_stmt : while comp_op COLON block'''
     p[0]=('while',p[2],p[4])
+# Định nghĩa lỗi: Nếu không phân tích được bất kỳ cú pháp nào đã được định nghĩa ở trên thì thông báo lỗi và kết thúc 
 def p_error(p):
     print("Syntax error")
     exit()
+#Kích hoạt Parser
 yacc.yacc()
+#Nhập các câu lệnh từ user
 lines = []
 while True:
     line = input()
@@ -127,5 +146,7 @@ while True:
     else:
         break
 text = '\n'.join(lines)
+# Phân tích cú pháp từ input
 result = yacc.parse(text)
+# Xuất kết quả phân tích
 print(result)
